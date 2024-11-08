03733333c759f978c03003d452eb76c4d1ce6442
kiki <demarchi.christiann@gmail.com>
2024-11-08 02:34:22 -0300

    new update

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -4,14 +4,20 @@ import {
   getSlices,
 } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
-
 import { components } from "../../slices";
 
-export default function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state);
+export async function getServerSideProps(context: any) {
+  const { state } = context.query;
+  const slices = await getSlices(state);
 
+  return {
+    props: {
+      slices,
+    },
+  };
+}
+
+export default function SliceSimulatorPage({ slices }: { slices: any[] }) {
   return (
     <SliceSimulator background="#121b2f">
       <SliceZone slices={slices} components={components} />
