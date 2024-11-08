af8b0f271bc3ba0513ddb034320156832f281a6f
kiki <demarchi.christiann@gmail.com>
2024-11-08 02:45:44 -0300

    l

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,30 +1,14 @@
-import {
-  SliceSimulator,
-  SliceSimulatorParams,
-  getSlices,
-} from "@slicemachine/adapter-next/simulator";
+"use client";
+
+import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
-import { redirect } from "next/navigation";
 
-import { components } from "@/slices";
+import { components } from "../../slices";
 
-export default function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams & { searchParams: { secret?: string } }) {
-  if (
-    process.env.SLICE_SIMULATOR_SECRET &&
-    searchParams.secret !== process.env.SLICE_SIMULATOR_SECRET
-  ) {
-    redirect("/");
-  }
-
-  const slices = getSlices(searchParams.state);
-
+export default function SliceSimulatorPage() {
   return (
-    <SliceSimulator background="" zIndex={10}>
-      <div className="max-h-[900px]">
-        <SliceZone slices={slices} components={components} />
-      </div>
-    </SliceSimulator>
+    <SliceSimulator
+      sliceZone={(props) => <SliceZone {...props} components={components} />}
+    />
   );
 }
