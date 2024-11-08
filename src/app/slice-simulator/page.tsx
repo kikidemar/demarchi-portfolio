53af4ea45c37a5ddd6b5698218723696c98772fc
kiki <demarchi.christiann@gmail.com>
2024-11-08 02:27:02 -0300

    try update

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -7,10 +7,10 @@ import { SliceZone } from "@prismicio/react";
 
 import { components } from "../../slices";
 
-export default function SliceSimulatorPage({
+export default async function SliceSimulatorPage({
   searchParams,
 }: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state);
+  const slices = await getSlices(searchParams.state);
 
   return (
     <SliceSimulator background="#121b2f">
