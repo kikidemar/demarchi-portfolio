64d24b28682fe506652c1b84be098e4de2a951b3
kiki <demarchi.christiann@gmail.com>
2024-11-08 02:56:49 -0300

    lt

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,18 +1,19 @@
-'use client'
-
 import {
   SliceSimulator,
   SliceSimulatorParams,
   getSlices,
 } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
-
 import { components } from "../../slices";
 
-export default function SliceSimulatorPage({
+export default async function SliceSimulatorPage({
   searchParams,
 }: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state);
+  // Esperar que la promesa se resuelva antes de obtener el valor de 'state'
+  const resolvedParams = await searchParams;
+
+  // Acceder al valor de 'state' después de resolver la promesa
+  const slices = getSlices(resolvedParams.state);
 
   return (
     <SliceSimulator background="#121b2f">
