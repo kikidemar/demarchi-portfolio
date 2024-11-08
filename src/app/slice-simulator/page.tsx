f870df5aa59fc70f2b4ca123e9befad0d9c7fed3
kiki <demarchi.christiann@gmail.com>
2024-11-08 02:49:09 -0300

    l

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
-
 import { components } from "../../slices";
 
 export default function SliceSimulatorPage() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Establecer isClient a true solo en el cliente
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Evita el renderizado en el servidor
+  }
+
   return (
     <SliceSimulator
       sliceZone={(props) => <SliceZone {...props} components={components} />}
