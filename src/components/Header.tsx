01dda3aa45d578a174ab089ee25e378afd8180fd
kiki <demarchi.christiann@gmail.com>
2024-11-08 03:36:03 -0300

    local finished. problems with deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from "react";
 import { createClient } from "@/prismicio";
 import NavBar from "@/components/NavBar";
--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
