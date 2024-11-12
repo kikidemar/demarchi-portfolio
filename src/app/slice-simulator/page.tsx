315b93e81fa02e73440026f4f874ddf66d3d98d4
kiki <demarchi.christiann@gmail.com>
2024-11-11 23:55:07 -0300

    new update

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -21,3 +21,26 @@ export default async function SliceSimulatorPage({
     </SliceSimulator>
   );
 }
+
+// Funcion Original
+
+// import {
+//   SliceSimulator,
+//   SliceSimulatorParams,
+//   getSlices,
+// } from "@slicemachine/adapter-next/simulator";
+// import { SliceZone } from "@prismicio/react";
+
+// import { components } from "../../slices";
+
+// export default function SliceSimulatorPage({
+//   searchParams,
+// }: SliceSimulatorParams) {
+//   const slices = getSlices(searchParams.state);
+
+//   return (
+//     <SliceSimulator background="#121b2f">
+//       <SliceZone slices={slices} components={components} />
+//     </SliceSimulator>
+//   );
+// }
